e3e0fe6c972dae0e2869f775a41e07f7ead7afe4
Kaloyan <kaloyan.sv.dimitrov@gmail.com>
2024-09-01 12:05:04 +0300

    Added a rainbow and increasing opacity effect to the tiles of the grid

--- logic.js
+++ logic.js
@@ -1,17 +1,32 @@
 const grid = document.querySelector("#grid-container");
+let colorInfo = [];
+let opacityInfo = [];
 
 for (let i = 0; i < 16; i++) {
     const column = document.createElement("div");
     column.classList.add("column");
+    colorInfo[i] = [];
+    opacityInfo[i] = [];
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "black";
+        const sqrBackground = document.createElement("div");
+        sqrBackground.classList.add("square-background");
+        sqrBackground.addEventListener("mouseenter", () => {
+            if (sqrBackground.style.backgroundColor == "") {
+                colorInfo[i][j] = "rgb(" + Math.random() * 255 + ", " + Math.random() * 255 + ", "+ Math.random() * 255 + ")";
+            }
+            opacityInfo[i][j] = opacityInfo[i][j] + 0.1;
+            sqrBackground.style.backgroundColor = "black";
+            sqrBackground.style.opacity = 1;
         });
-        square.addEventListener("mouseleave", () => {
-            square.style.backgroundColor = "grey";
+        sqrBackground.addEventListener("mouseleave", () => {
+            sqrBackground.style.backgroundColor = colorInfo[i][j];
+            sqrBackground.style.opacity = opacityInfo[i][j];
         })
+        square.appendChild(sqrBackground);
+        colorInfo[i][j] = sqrBackground.style.backgroundColor;
+        opacityInfo[i][j] = 0;
         column.appendChild(square);
     }
     grid.appendChild(column);
@@ -27,15 +42,28 @@ function resizeGrid() {
         for (let i = 0; i < newSize; i++) {
             const column = document.createElement("div");
             column.classList.add("column");
+            colorInfo[i] = [];
+            opacityInfo[i] = [];
             for (let j = 0; j < newSize; j++) {
                 const square = document.createElement("div");
                 square.classList.add("square");
-                square.addEventListener("mouseenter", () => {
-                square.style.backgroundColor = "black";
+                const sqrBackground = document.createElement("div");
+                sqrBackground.classList.add("square-background");
+                sqrBackground.addEventListener("mouseenter", () => {
+                    if (sqrBackground.style.backgroundColor == "") {
+                        colorInfo[i][j] = "rgb(" + Math.random() * 255 + ", " + Math.random() * 255 + ", "+ Math.random() * 255 + ")";
+                    }
+                    opacityInfo[i][j] = opacityInfo[i][j] + 0.1;
+                    sqrBackground.style.backgroundColor = "black";
+                    sqrBackground.style.opacity = 1;
                 });
-                square.addEventListener("mouseleave", () => {
-                square.style.backgroundColor = "grey";
+                sqrBackground.addEventListener("mouseleave", () => {
+                    sqrBackground.style.backgroundColor = colorInfo[i][j];
+                    sqrBackground.style.opacity = opacityInfo[i][j];
                 })
+                square.appendChild(sqrBackground);
+                colorInfo[i][j] = sqrBackground.style.backgroundColor;
+                opacityInfo[i][j] = 0;
                 column.appendChild(square);
             }
             grid.appendChild(column);
